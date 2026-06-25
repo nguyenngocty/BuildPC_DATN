9bda57b9c59e055b256dad27d387aee0b57ca41e
chuanvmpc09137 <vmchuan25@gmail.com>
2026-06-26 00:51:35 +0700

    [Admin] - Code hoàn chỉnh nhất

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,27 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 
-import Checkout from "../views/pages/Checkout/Checkout";
-import Blog from "../views/pages/Blog/Blog";
 import OrderSuccess from "../views/pages/OrderSuccess/OrderSuccess";
 import Home from "../views/pages/Home/Home";
 import Auth from "../views/pages/Auth/Auth";
 import Cart from "../views/pages/Cart/Cart";
+import About from "../views/pages/About/About";
 import Checkout from "../views/pages/Checkout/Checkout";
-import OrderSuccess from "../views/pages/OrderSuccess/OrderSuccess";
 import AdminRoutes from "./AdminRoutes";
 
 function AppRoutes() {
-  return (
+  return (  
     <Routes>
       {/* Trang khách */}
       <Route path="/" element={<Home />} />
-      <Route path="/checkout" element={<Checkout />} />
       <Route path="/order-success" element={<OrderSuccess />} />
-      <Route path="/blog" element={<Blog />} />
       <Route path="/auth" element={<Auth />} />
       <Route path="/cart" element={<Cart />} />
+      <Route path="/about" element={<About />} />
       <Route path="/checkout" element={<Checkout />} />
-      <Route path="/order-success" element={<OrderSuccess />} />
 
       {/* Toàn bộ trang quản trị */}
       <Route path="/admin/*" element={<AdminRoutes />} />
