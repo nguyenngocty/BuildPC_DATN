2430cbce6ab7ecd0dce225fb253e4b9083f21f50
chuanvmpc09137 <vmchuan25@gmail.com>
2026-06-25 23:34:18 +0700

    khongcogi

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,20 +1,25 @@
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-import AdminRoutes from "./AdminRoutes";
-import Checkout from "../views/pages/Checkout/Checkout";
 import Home from "../views/pages/Home/Home";
 import Auth from "../views/pages/Auth/Auth";
 import Cart from "../views/pages/Cart/Cart";
+import Checkout from "../views/pages/Checkout/Checkout";
 import AdminRoutes from "./AdminRoutes";
+
 function AppRoutes() {
   return (
     <Routes>
+      {/* Trang khách */}
       <Route path="/" element={<Home />} />
-      <Route path="/admin/*" element={<AdminRoutes />} />
-      <Route path="/checkout" element={<Checkout />} />
       <Route path="/auth" element={<Auth />} />
       <Route path="/cart" element={<Cart />} />
+      <Route path="/checkout" element={<Checkout />} />
+
+      {/* Toàn bộ trang quản trị */}
       <Route path="/admin/*" element={<AdminRoutes />} />
+
+      {/* Khi nhập đường dẫn không tồn tại */}
+      <Route path="*" element={<Home />} />
     </Routes>
   );
 }
