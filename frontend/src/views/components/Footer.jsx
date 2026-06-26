1755b3d06392aa71bab659769e70cc3b1e2e32c1
nguyenngocty <nty92355@gmail.com>
2026-06-26 12:51:46 +0700

    [Client] - Hoàn chỉnh lại footer đầy đủ

--- frontend/src/views/components/Footer.jsx
+++ frontend/src/views/components/Footer.jsx
@@ -1,38 +1,46 @@
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="container">
-        <div className="row g-4">
-          <div className="col-md-4">
-            <h4>BuildPC</h4>
-            <p>Build PC theo ý bạn - Linh kiện chính hãng, giá tốt nhất.</p>
+    <footer className="app-footer">
+      <div className="footer-container">
+        <div className="footer-grid">
+          <div className="footer-col">
+            <h4 className="footer-brand">BuildPC</h4>
+            <p className="footer-desc">
+              Build PC theo ý bạn - Linh kiện chính hãng, giá tốt nhất.
+            </p>
           </div>
 
-          <div className="col-md-2">
-            <h6>Chính sách</h6>
-            <p>Bảo hành</p>
-            <p>Đổi trả</p>
-            <p>Thanh toán</p>
+          <div className="footer-col">
+            <h6 className="footer-title">Chính sách</h6>
+            <ul className="footer-list">
+              <li>Bảo hành</li>
+              <li>Đổi trả</li>
+              <li>Thanh toán</li>
+            </ul>
           </div>
 
-          <div className="col-md-3">
-            <h6>Hỗ trợ</h6>
-            <p>Tra cứu đơn hàng</p>
-            <p>Tư vấn build PC</p>
-            <p>Liên hệ</p>
+          <div className="footer-col">
+            <h6 className="footer-title">Hỗ trợ</h6>
+            <ul className="footer-list">
+              <li>Tra cứu đơn hàng</li>
+              <li>Tư vấn build PC</li>
+              <li>Liên hệ</li>
+            </ul>
           </div>
 
-          <div className="col-md-3">
-            <h6>Nhận ưu đãi</h6>
-            <div className="newsletter">
-              <input placeholder="Email của bạn..." />
+          <div className="footer-col">
+            <h6 className="footer-title">Nhận ưu đãi</h6>
+
+            <div className="footer-newsletter">
+              <input type="email" placeholder="Email của bạn..." />
               <button>Đăng ký</button>
             </div>
           </div>
         </div>
 
-        <hr />
-        <p className="copyright">© 2026 BuildPC. All rights reserved.</p>
+        <div className="footer-bottom">
+          <p>© 2026 BuildPC. All rights reserved.</p>
+        </div>
       </div>
     </footer>
   );
